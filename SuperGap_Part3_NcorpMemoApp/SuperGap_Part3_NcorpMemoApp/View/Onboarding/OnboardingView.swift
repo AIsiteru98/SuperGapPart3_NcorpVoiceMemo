b3978992980e949bd29a16664c938450be8005f0
AIsiteru98 <132365672+AIsiteru98@users.noreply.github.com>
2024-10-16 04:14:21 +0900

    init setting

---
+++ SuperGap_Part3_NcorpMemoApp/SuperGap_Part3_NcorpMemoApp/View/Onboarding/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  SuperGap_Part3_NcorpMemoApp
+//
+//  Created by 이재용 on 10/14/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
