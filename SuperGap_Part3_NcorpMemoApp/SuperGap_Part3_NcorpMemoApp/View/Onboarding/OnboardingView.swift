1e1daa06f80e2126489c2ff59801ea62b4d37558
AIsiteru98 <132365672+AIsiteru98@users.noreply.github.com>
2024-10-18 22:00:17 +0900

    #1-onboarding fin

--- SuperGap_Part3_NcorpMemoApp/SuperGap_Part3_NcorpMemoApp/View/Onboarding/OnboardingView.swift
+++ SuperGap_Part3_NcorpMemoApp/SuperGap_Part3_NcorpMemoApp/View/Onboarding/OnboardingView.swift
@@ -12,11 +12,30 @@ struct OnboardingView: View {
     // 온보딩 뷰 생성시 최초 1회 생성후 라이프사이클 동안 공유됨
     // StateObjcect로 관리되는 객체가 변경될 때마다. 뷰는 새로 랜더링 됨
     @StateObject private var onboardingViewModel: OnboardingViewModel = OnboardingViewModel()
-    
+    @StateObject private var pathModel = PathModel()
     
     var body: some View {
         // TODO: - 화면 전환 구현 필요
-        OnboardingContentView(onboardingViewModel: onboardingViewModel)
+        NavigationStack(path: $pathModel.paths) {
+            OnboardingContentView(onboardingViewModel: onboardingViewModel)
+            //navigaionDestination은 해쉬어블을 채택한 경우만 사용가능하므로 패스타입에 추가함
+                .navigationDestination(
+                    for: PathType.self) { path in
+                        switch path {
+                        case .homeView:
+                            HomeView()
+                                .navigationBarBackButtonHidden()
+                        case .memoView:
+                            MemoView()
+                                .navigationBarBackButtonHidden()
+                        case.todoView:
+                            TodoView()
+                                .navigationBarBackButtonHidden()
+                        }
+                    }
+        }
+        //네비게이션 스택에 대해 환경객체 제공
+        .environmentObject(pathModel)
     }
 }
 
@@ -90,9 +109,11 @@ private struct OnboardingCellView: View {
             Image(onboardingContent.imageName)
                 .resizable()
                 .scaledToFit()
+                .padding(.top, 40)
             
             
             HStack {
+                // 중앙 정렬을 위한 스페이서 1
                 Spacer()
                 
                 VStack {
@@ -107,22 +128,28 @@ private struct OnboardingCellView: View {
                     Text(onboardingContent.subtitle)
                         .font(.system(size: 16))
                 }
-                
+                // 중앙 정렬을 위한 스페이서 2
                 Spacer()
                 
             }
             .background(.white)
             .cornerRadius(0)
+            .shadow(radius: 10)
+            
         }
-        .shadow(radius: 10)
+        
     }
 }
 
 // MARK: - 시작하기 버튼 뷰
 private struct StartButtonView: View {
+    // 다른 뷰에서 관리하는 곳에 접근하려면 어떻게 할까?
+    // 상위 뷰의 정보를 주입받으면 된다
+    // 네비게이션 스택처럼 뷰의 전환 전체를 컨트롤하는 경우, environmentObject로 전역으로 컨트롤 하면 된다.
+    @EnvironmentObject private var pathModel : PathModel
     fileprivate var body: some View {
         Button {
-            print("start")
+            pathModel.paths.append(.homeView)
         } label: {
             Text("시작하기 ->")
                 .font(.system(size: 16, weight: .medium))
@@ -132,6 +159,16 @@ private struct StartButtonView: View {
         .padding(.bottom, 50)
     }
 }
+/*
+ @EnvironmentObject의 장점
+ 글로벌 상태 관리:
+ @EnvironmentObject는 뷰의 상위에서 설정된 객체를 하위 뷰로 자동적으로 전달할 수 있기 때문에, 뷰 계층 구조에서 여러 뷰가 같은 데이터를 공유해야 할 때 매우 유용합니다.
+ 예를 들어, OnboardingView에서 StartButtonView가 같은 PathModel 데이터를 참조하고 싶을 때, 일일이 @Binding으로 전달할 필요 없이 @EnvironmentObject로 쉽게 접근할 수 있습니다.
+ 데이터 전달의 단순화:
+ 상위 뷰에서 하위 뷰로 데이터를 전달할 때, @Binding을 사용하면 모든 뷰 계층을 통해 데이터를 전달해야 하지만, @EnvironmentObject는 그럴 필요 없이 직접적으로 접근할 수 있습니다. 중간에 있는 뷰들에서 데이터를 전달하는 코드를 작성하지 않아도 됩니다.
+ 동일한 객체를 여러 뷰에서 사용할 때 편리:
+ @EnvironmentObject는 여러 뷰에서 공유해야 하는 데이터 모델에 적합합니다. OnboardingView와 같은 상위 뷰에서 PathModel을 주입하면, 하위 뷰들(예: StartButtonView, OnboardingContentView)에서 해당 데이터를 간편하게 사용할 수 있습니다.
+ */
 
 #Preview {
     OnboardingView()
---
+++ SuperGap_Part3_NcorpMemoApp/SuperGap_Part3_NcorpMemoApp/View/Todo/TodoView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SuperGap_Part3_NcorpMemoApp
+//
+//  Created by 이재용 on 10/18/24.
+//
+
+import SwiftUI
+
+struct TodoView: View {
+    var body: some View {
+        Text("Todo")
+    }
+}
+
+#Preview {
+    TodoView()
+}
